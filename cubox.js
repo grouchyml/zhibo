c628370f38d3eb28a003df7df1c990b03a36d989
grouchyml <64346536+grouchyml@users.noreply.github.com>
2023-02-23 09:53:27 +0800

    Update cubox.js

--- cubox.js
+++ cubox.js
@@ -37,6 +37,8 @@ if (url.indexOf(vip) != -1) {
     obj.data.isExpire = false;
     obj.data.active = true;
     obj.data.payTime = 1660006006;
+    obj.data.nickName = "彭于晏解锁";
+    obj.data.thirdNickName = "彭于晏解锁";
 
 	body = JSON.stringify(obj);
 }
